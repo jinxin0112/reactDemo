dedbf6e2d59758f8087183deb7949e0ceb503ddb
kingduidui <m18508218948@163.com>
2018-02-27 15:44:07 +0800

    component of ImageFigure

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import React from 'react';
 import imageDatas from '../data/imageDatas';
 
 //利用自执行函数获取图片信息
-let imgDatas = ((imgArr)=>{
+const imgDatas = ((imgArr)=>{
 	for(let i=0;i<imgArr.length;i++){
 		let singleImageData = imgArr[i];
 		singleImageData.imgUrl = require('../images/'+imgArr[i].fileName);
@@ -14,13 +14,40 @@ let imgDatas = ((imgArr)=>{
 	return imgArr;
 })(imageDatas);
 
+class ImgFigure extends React.Component {
+	
+	constructor(props){
+		super(props);
+		this.state = props;
+	}
+	
+	render () {
+		return (
+			<figure className="img-figure">
+				<img className="figure-img" src={this.state.data.imgUrl} alt={this.state.data.title}/>
+				<h2 className="img-title">{this.state.data.title}</h2>
+			</figure>
+		)
+	}
+}
+
 class AppComponent extends React.Component {
   render() {
+  	
+  	let controllerUnits = [],
+  		imgFigures = [];
+  		 		
+  	imgDatas.forEach((value)=>{
+  		imgFigures.push(<ImgFigure data={value}/>);
+  	});
+
     return (
 		<section className="stage">
 			<section className="img-sec">
+				{imgFigures}
 			</section>
 			<nav className="controller-nav">
+				{controllerUnits}
 			</nav>
 		</section>
     );
--- src/data/imageDatas.js
+++ src/data/imageDatas.js
@@ -1,34 +1,33 @@
-export default 
-	[
+export default[
 		{
 			fileName:'1.jpg',
 			title:'love make better',
 			desc:'this is why we love'
-		},		{
+		},{
 			fileName:'2.jpg',
 			title:'love make better',
 			desc:'this is why we love'
-		},		{
+		},{
 			fileName:'3.jpg',
 			title:'love make better',
 			desc:'this is why we love'
-		},		{
+		},{
 			fileName:'4.jpg',
 			title:'love make better',
 			desc:'this is why we love'
-		},		{
+		},{
 			fileName:'5.jpg',
 			title:'love make better',
 			desc:'this is why we love'
-		},		{
+		},{
 			fileName:'6.jpg',
 			title:'love make better',
 			desc:'this is why we love'
-		},		{
+		},{
 			fileName:'7.jpg',
 			title:'love make better',
 			desc:'this is why we love'
-		},		{
+		},{
 			fileName:'8.jpg',
 			title:'love make better',
 			desc:'this is why we love'
